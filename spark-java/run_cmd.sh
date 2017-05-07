c5368e5fd1bde88fca3be5d300fb87365dc4df81
Satish Kumar <sateeshfrnd@gmail.com>
2017-05-07 10:41:17 +0530

    Code Clean up

--- spark-java/run_cmd.sh
+++ spark-java/run_cmd.sh
@@ -1,4 +1,4 @@
-spark-submit --class com.satish.workshop.spark.WordCount \
+spark-submit --class com.satish.learnings.spark_java.spark.WordCount \
 	--master local[*] \
 	target/spark-java-maven-0.0.1-SNAPSHOT.jar \
 	'/home/satish/MyWorkshop/Sample-Inputs/wordcount-Input.txt' \
