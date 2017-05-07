9f8962199e1ab2fd7450a6e5686481b7761463fd
Satish Kumar <sateeshfrnd@gmail.com>
2017-05-07 10:38:08 +0530

    Spark Java Maven project

---
+++ spark-java/src/main/java/com/satish/learnings/spark_java/spark/WordCount.java
@@ -0,0 +1,85 @@
+package com.satish.learnings.spark_java.spark;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+import static java.lang.System.out;
+
+/**
+ * @author SatishKumar
+ *
+ */
+public class WordCount {
+
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(WordCount.class);
+
+		if (args.length < 2) {
+			logger.error("=> wrong parameters number");
+			System.out.println("Usage: Pass Arguments <InputFile-Path> <OutputFile-Path>");
+			System.exit(1);
+		}
+
+		String inputFile = args[0];
+		String outputFile = args[1];
+
+		// Create a Java Spark Context.
+		SparkConf conf = new SparkConf().setAppName("wordCount");
+		// conf.setMaster("local");
+
+		// Create SparkContext
+		JavaSparkContext jsc = new JavaSparkContext(conf);
+
+		logger.info("=> inputPath \"" + inputFile + "\"");
+
+		// Load input data.
+		JavaRDD<String> lines = jsc.textFile(inputFile);
+
+		logger.info("=> numLines \"" + lines.count() + "\"");
+
+		// Split up into words.
+		JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+
+			public Iterator<String> call(String line) throws Exception {
+				return (Iterator<String>) Arrays.asList(line.split(","));
+			}
+			
+		}); 
+		
+		// Transform into word and count.
+		JavaPairRDD<String, Integer> wordcount = words.mapToPair(new PairFunction<String, String, Integer>() {
+
+			public Tuple2<String, Integer> call(String word) throws Exception {
+				return new Tuple2<String, Integer>(word, 1);
+			}
+
+		});
+
+		JavaPairRDD<String, Integer> aggreatedWordcount = wordcount
+				.reduceByKey(new Function2<Integer, Integer, Integer>() {
+
+					public Integer call(Integer x, Integer y) throws Exception {
+						return (x + y);
+					}
+				});
+
+		List<Tuple2<String, Integer>> result = aggreatedWordcount.collect();
+		out.println("result = " + result);
+
+		// Save the word count back out to a text file, causing evaluation.
+		aggreatedWordcount.saveAsTextFile(outputFile);
+
+		jsc.stop();
+
+	}
+
+}
